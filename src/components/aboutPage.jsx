3ba6721f150391eded987b28f1b539cd3897d7ea
Megan Carlan <mgcarlan@gmail.com>
2023-03-01 05:18:29 -0600

    updated formatting on About page and changed the formatting of home page

--- src/components/aboutPage.jsx
+++ src/components/aboutPage.jsx
@@ -2,18 +2,18 @@
 
 export function AboutPage() {
     return (
-     <div>
-        <h1>About Me</h1>
-        <p>
+     <div className="AboutPage">
+        <h1 className="green">About Me</h1>
+        <p className="lightestSlate">
             Hello! My name is Brittany and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
         </p>
-        <p>
+        <p className="lightestSlate">
             Fast-forward to today, and I have had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.
         </p>
-        <p>
+        <p className="green">
             Here are a few technologies I have been working with recently:
         </p>
-        <ul>
+        <ul className="lightestSlate">
             <li> JavaScript (ES6+)</li>
             <li>React</li>
             <li>Node.js</li>
@@ -21,7 +21,7 @@ export function AboutPage() {
             <li>MYSQL</li>
             <li>WordPress</li>
         </ul>
-        <img src={require("/Users/mcarlan/Desktop/repos/portfolio/src/assets/headshot.png")} alt="My Image" />
+        <img className="headshot" src={require("/Users/mcarlan/Desktop/repos/portfolio/src/assets/headshot.png")} alt="My Image" />
 
     </div>
     )
